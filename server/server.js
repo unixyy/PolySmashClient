5f8dee9ea65d1733dadc9228a6a5cb22c3467ff8
Richard MARTIN <rclic66000@gmail.com>
2022-03-30 01:04:25 +0200

    repairing

--- server/server.js
+++ server/server.js
@@ -5,7 +5,7 @@ const publicPath = path.join(__dirname, "..", "public");
 const port = process.env.PORT || 3000;
 app.use(express.static(publicPath));
 app.get("*", (req, res) => {
-  res.sendFile(path.join(publicPath, "./public/index.html"));
+  res.sendFile(path.join(publicPath, "index.html"));
 });
 app.listen(port, () => {
   console.log("Server is up!");
